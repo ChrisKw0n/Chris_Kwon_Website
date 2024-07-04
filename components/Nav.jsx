dad4f8645f447c814f77140529347d106a560f90
hyukjun3 <ckwo873@aucklanduni.ac.nz>
2024-07-04 17:59:57 +1200

    Refined nav bar for aesthetic

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -9,29 +9,35 @@ const links = [
     path: "/",
   },
   {
-    name: "About",
+    name: "Portfolio",
     path: "/about",
   },
   {
-    name: "Projects",
+    name: "Resume",
     path: "/projects",
-  },
-  {
-    name: "Contact",
-    path: "/contact",
   },
 ];
 
 const Nav = () => {
+  const pathname = usePathname();
   return (
-    <nav>
-      {links.map((link, index) => {
-        return (
-          <Link href={link.path} key={index}>
+    <nav className="flex items-center">
+      {links.map((link, index) => (
+        <div key={index} className="flex items-center">
+          <Link
+            href={link.path}
+            className={`${
+              link.path === pathname &&
+              "text-orange-400 border-b-2 border-orange-400"
+            } capitalize font-medium hover:text-orange-400 transition-all`}
+          >
             {link.name}
           </Link>
-        );
-      })}
+          {index < links.length - 1 && (
+            <span className="mx-2 text-gray-400">//</span>
+          )}
+        </div>
+      ))}
     </nav>
   );
 };

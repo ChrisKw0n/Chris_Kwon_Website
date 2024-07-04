6a354fcc42719d6de653c58ff4a9a81d6538e88d
hyukjun3 <ckwo873@aucklanduni.ac.nz>
2024-07-04 16:58:11 +1200

    Added desktop nav bar options. Also added mobile nav bar for smaller screen sizes

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,17 +7,24 @@ import Nav from "./Nav";
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
-      <div className="container mx-auto">
+      <div className="container mx-auto flex justify-between items-center">
         {/* Logo */}
         <Link href="/">
           <h1 className="text-4xl front-semibold">
             Chris<span className="text-orange-400">.</span>
           </h1>
         </Link>
-        {/* Navigation */}
-        <div className="hidden xl:flex">
+
+        {/* Desktop Nav */}
+        <div className="hidden xl:flex items-center gap-8">
           <Nav />
+          <Link href="/contact">
+            <Button>Contact me!</Button>
+          </Link>
         </div>
+
+        {/* Mobile Nav */}
+        <div className="xl:hidden">mobile nav</div>
       </div>
     </header>
   );
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,5 +1,39 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "About",
+    path: "/about",
+  },
+  {
+    name: "Projects",
+    path: "/projects",
+  },
+  {
+    name: "Contact",
+    path: "/contact",
+  },
+];
+
 const Nav = () => {
-  return <nav>desktop nav</nav>;
+  return (
+    <nav>
+      {links.map((link, index) => {
+        return (
+          <Link href={link.path} key={index}>
+            {link.name}
+          </Link>
+        );
+      })}
+    </nav>
+  );
 };
 
 export default Nav;

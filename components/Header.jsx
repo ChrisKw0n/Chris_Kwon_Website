dd4bf7f59459e0d53f9d1dd960a3207bebb44a35
hyukjun3 <ckwo873@aucklanduni.ac.nz>
2024-07-04 16:01:49 +1200

    Added a navigation bar under header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 
+// components
+import Nav from "./Nav";
+
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
@@ -8,9 +11,13 @@ const Header = () => {
         {/* Logo */}
         <Link href="/">
           <h1 className="text-4xl front-semibold">
-            Chris<span>.</span>
+            Chris<span className="text-orange-400">.</span>
           </h1>
         </Link>
+        {/* Navigation */}
+        <div className="hidden xl:flex">
+          <Nav />
+        </div>
       </div>
     </header>
   );

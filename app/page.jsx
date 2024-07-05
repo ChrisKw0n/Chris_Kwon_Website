3c12bc99a594c5150aed7a407a6fa16920ae7e39
hyukjun3 <ckwo873@aucklanduni.ac.nz>
2024-07-05 13:28:08 +1200

    Implemented an interactive bubble that tracks the cursor

--- app/page.jsx
+++ app/page.jsx
@@ -2,18 +2,21 @@ import React from "react";
 import "./globals.scss";
 
 // components
+
 import Header from "../components/Header";
+import Interactive from "../components/Interactive";
 import PageTransition from "../components/PageTransition";
 
 const Home = () => {
   return (
     <div className="gradient-bg">
-      <Header />
       <div class="g1"></div>
-      <div class="g2"></div>
+      {/* <div class="g2"></div>
       <div class="g3"></div>
       <div class="g4"></div>
-      <div class="g5"></div>
+      <div class="g5"></div> */}
+      <Header />
+      <Interactive />
       <PageTransition />
     </div>
   );
---
+++ components/Interactive.jsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React, { useEffect } from "react";
+import "../app/globals.scss";
+
+const InteractiveComponent = () => {
+  useEffect(() => {
+    let curx = 0;
+    let cury = 0;
+    let tgX = 0;
+    let tgY = 0;
+
+    const interBubble = document.querySelector(".interactive");
+
+    if (!interBubble) {
+      console.error("Interactive element not found");
+      return;
+    }
+
+    function move() {
+      curx += (tgX - curx) / 20;
+      cury += (tgY - cury) / 20;
+      interBubble.style.transform = `translate(${Math.round(
+        curx
+      )}px, ${Math.round(cury)}px)`;
+      requestAnimationFrame(move);
+    }
+
+    window.addEventListener("mousemove", (event) => {
+      tgX = event.clientX;
+      tgY = event.clientY;
+    });
+
+    move();
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("mousemove", (event) => {
+        tgX = event.clientX;
+        tgY = event.clientY;
+      });
+    };
+  }, []);
+
+  return <div className="interactive"></div>;
+};
+
+export default InteractiveComponent;

a95f2f7eb684330056c44edc8e417f9c0985b4ff
hyukjun3 <ckwo873@aucklanduni.ac.nz>
2024-08-28 00:01:49 +1200

    refactor: Refactored sliding text into component.

--- app/page.jsx
+++ app/page.jsx
@@ -1,61 +1,17 @@
 "use client";
 
 import React, { useEffect, useRef } from "react";
-import { useScroll, motion } from "framer-motion";
-import Lenis from "lenis";
 
 import TypeEffectText from "../components/home/typeEffectText";
 import SocialLinks from "../components/home/socialLinks";
-import Slide from "../components/home/slide";
-import Picture1 from "../components/images/github_logo.png";
-import Picture2 from "../components/images/github_logo.png";
-import Picture3 from "../components/images/github_logo.png";
+import SlideComponent from "../components/home/slideComponent/slideComponent";
 
 const Home = () => {
-  const container = useRef();
-
-  const { scrollYProgress } = useScroll({
-    target: container,
-    offset: ["start end", "end start"],
-  });
-
-  useEffect(() => {
-    const lenis = new Lenis();
-
-    function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
-    requestAnimationFrame(raf);
-  }, []);
-
   return (
-    <main className="overflow-hidden">
-      <div>
-        <SocialLinks />
-        <TypeEffectText />
-      </div>
-      <div className="h-[100vh]" />
-      <Slide
-        src={Picture1}
-        direction={"left"}
-        left={"-40%"}
-        progress={scrollYProgress}
-      />
-      <Slide
-        src={Picture2}
-        direction={"right"}
-        left={"-25%"}
-        progress={scrollYProgress}
-      />
-      <Slide
-        src={Picture3}
-        direction={"left"}
-        left={"-75%"}
-        progress={scrollYProgress}
-      />
-      <div className="h-[100vh]" />
+    <main>
+      <SocialLinks />
+      <TypeEffectText />
+      <SlideComponent />
     </main>
   );
 };
--- components/home/slideComponent/slide.jsx
+++ components/home/slideComponent/slide.jsx
@@ -26,9 +26,9 @@ const Slide = (props) => {
 
 const Phrase = ({ src }) => {
   return (
-    <div className={"px-5 flex gap-5 items-center"}>
-      <p className="text-[7.5vw]">Front End Developer</p>
-      <span className="relative h-[7.5vw] aspect-[4/2] rounded-full overflow-hidden">
+    <div className={"px-2 flex gap-3 items-center"}>
+      <p className="text-[4vw]">Web 3.0 Developer</p>
+      <span className="relative h-[4vw] aspect-[4/2] rounded-full overflow-hidden">
         <Image style={{ objectFit: "cover" }} src={src} alt="image" fill />
       </span>
     </div>

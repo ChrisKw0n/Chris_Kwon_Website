964e32c3f93fd374772b37465f4de76bf66d298a
hyukjun3 <ckwo873@aucklanduni.ac.nz>
2024-12-11 23:23:58 +1300

    feat: Implemented glass torus.

--- app/page.jsx
+++ app/page.jsx
@@ -5,12 +5,14 @@ import React, { useEffect, useRef } from "react";
 import TypeEffectText from "../components/home/typeEffectText";
 import SocialLinks from "../components/home/socialLinks";
 import SlideComponent from "../components/home/slideComponent/slideComponent";
+import GlassComponent from "../components/home/glassComponent/glassComponent";
 
 const Home = () => {
   return (
     <main>
-      <SocialLinks />
       <TypeEffectText />
+      <SocialLinks />
+      <GlassComponent />
       <SlideComponent />
     </main>
   );
